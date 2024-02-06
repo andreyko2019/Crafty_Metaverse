72dbcfd6176a13e77d2f48f51ed9434e2a2b74bb
andreyko2019 <vip.andreyko@gmail.com>
2024-02-06 16:15:01 +0200

    init project

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<script setup>
+import Header from '@/components/header.vue'
+
+</script>
+
+<template>
+  <Header></Header>
+</template>
+
+<style lang="scss" >
+  @import "@/scss/normalize.scss";
+</style>
---
+++ src/components/header.vue
@@ -0,0 +1,28 @@
+<template>
+    <header class="header">
+        <div class="header__container">
+            <a class="header__logo" href="#">
+                <svg class="header__logo-icon">
+                    <use xlink:href="@/assets/sprite.svg#logo"></use>
+                </svg>
+            </a>
+            <nav class="header__navigation">
+                <a class="header__navigation-button" href="#">Home</a>
+                <a class="header__navigation-button" href="#">About Us</a>
+                <a class="header__navigation-button" href="#">White paper</a>
+                <a class="header__navigation-button" href="#">Tokenomics</a>
+                <a class="header__navigation-button" href="#">Road Map</a>
+                <a class="header__navigation-button" href="#">How to buy</a>
+                <a class="header__navigation-button" href="#">Buy</a>
+            </nav>
+        </div>
+    </header>
+</template>
+
+<script setup>
+    
+</script>
+
+<style scoped>
+ @import '@/scss/components/header.scss';
+</style>
