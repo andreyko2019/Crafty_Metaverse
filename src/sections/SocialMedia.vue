f66b27429c731b8e202d57127c177a5ac6f72df7
andreyko2019 <vip.andreyko@gmail.com>
2024-02-08 11:39:25 +0200

    adaptive social media section

--- src/sections/SocialMedia.vue
+++ src/sections/SocialMedia.vue
@@ -1,30 +1,63 @@
 <template>
   <section class="social-media-section">
+    <img
+      class="social-media-section__background"
+      src="@/assets/images/socialMediaBackground.png"
+      alt="Background"
+    />
     <div class="social-media-section__container">
       <SectionTitle title="Social media" titleDescription="Questions" />
-      <Swiper
-        class="social-media-section__swiper"
-        :slidesPerView="4"
-        spaceBetween="0"
-        :allowTouchMove="false"
-      >
-        <SwiperSlide class="social-media-section__swiper-slide"
-          ><a class="social-media-section__link" href="https://twitter.com/"
-            ><img src="@/assets/images/Twitter.png" alt="" /></a
+      <Swiper class="social-media-section__swiper" v-bind="swiperOptions">
+        <SwiperSlide
+          class="social-media-section__swiper-slide social-media-section__swiper-slide_twitter"
+          ><a
+            class="social-media-section__link social-media-section__link"
+            href="https://twitter.com/"
+          >
+            <div class="social-media-section__link-img-container">
+              <img
+                class="social-media-section__link-img"
+                src="@/assets/images/Twitter.png"
+                alt=""
+              /></div></a
         ></SwiperSlide>
         <SwiperSlide
+          class="social-media-section__swiper-slide social-media-section__swiper-slide_telegram"
           ><a
             class="social-media-section__link"
             href="https://web.telegram.org/"
-            ><img src="@/assets/images/Telegram.png" alt="" /></a
+          >
+            <div class="social-media-section__link-img-container">
+              <img
+                class="social-media-section__link-img"
+                src="@/assets/images/Telegram.png"
+                alt=""
+              /></div></a
         ></SwiperSlide>
         <SwiperSlide
-          ><a class="social-media-section__link" href="https://www.youtube.com/"
-            ><img src="@/assets/images/YouTube.png" alt="" /></a
+          class="social-media-section__swiper-slide social-media-section__swiper-slide_youtube"
+          ><a
+            class="social-media-section__link social-media-section__link"
+            href="https://www.youtube.com/"
+          >
+            <div class="social-media-section__link-img-container">
+              <img
+                class="social-media-section__link-img"
+                src="@/assets/images/YouTube.png"
+                alt=""
+              /></div></a
         ></SwiperSlide>
         <SwiperSlide
-          ><a class="social-media-section__link" href="https://discord.com/"
-            ><img src="@/assets/images/Discord.png" alt="" /></a
+          class="social-media-section__swiper-slide social-media-section__swiper-slide_discord"
+          ><a
+            class="social-media-section__link social-media-section__link"
+            href="https://discord.com/"
+            ><div class="social-media-section__link-img-container">
+              <img
+                class="social-media-section__link-img"
+                src="@/assets/images/Discord.png"
+                alt=""
+              /></div></a
         ></SwiperSlide>
       </Swiper>
     </div>
@@ -35,6 +68,22 @@
 import SectionTitle from '@/components/SectionTitle.vue'
 import { Swiper, SwiperSlide } from 'swiper/vue'
 import 'swiper/swiper-bundle.css'
+
+const swiperOptions = {
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+      allowTouchMove: true,
+    },
+    390: {
+      slidesPerView: 'auto',
+    },
+    1024: {
+      slidesPerView: 4,
+      allowTouchMove: false,
+    },
+  },
+}
 </script>
 
 <style lang="scss">
